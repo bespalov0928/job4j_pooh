4d0f6aeaea6e63ff0f5f0d01d447d0e0b233ac3a
Dmitry-Bespalov <mailto:bespalov0928@mail.ru>
2021-03-03 06:06:13 +0300

    3/1/7/1. Техническое задание - проект "Pooh JMS" [#444810]

---
+++ src/main/java/Demo.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.Executors;
+
+public class Demo {
+    public static void main(String[] args) throws IOException {
+        Executors.newFixedThreadPool(2);
+        Server server = new Server();
+        //запускаем сервер и ждем пакеты
+        Thread thredServer = new Server();
+       thredServer.start();
+
+
+        //клиент отправляет пакет  на сервер
+        System.out.println("firstClient");
+        Thread thredClient = new Thread(
+                () -> {
+                    System.out.println(thredServer.getState());
+                    try {
+                        new Client("localhost", 5006).start();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    //System.out.println("server.start()");
+                    //server.start();
+                });
+        thredClient.start();
+
+        //клиент получает ответ от сервера
+        //Client consumer = new Client(new Socket("localhost", 5000));
+    }
+}
